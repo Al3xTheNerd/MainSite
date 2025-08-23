4c608d41c83b74a6033cdd0feb357f6c981ff98c
Joshua Deason <joshua.deason@zoho.com>
2025-08-23 14:58:45 -0600

    Add test route to test admin only decorator.

--- core/routes/public.py
+++ core/routes/public.py
@@ -1,8 +1,16 @@
 from flask import render_template
 from core import app
-
+from core.decorators.auth import admin_required
+from flask_login import login_required
 
 
 @app.route('/', methods=('GET', 'POST'))
 def index(): 
+    return render_template("public/index.html")
+
+
+@app.route('/test', methods=('GET', 'POST'))
+@login_required
+@admin_required()
+def test(): 
     return render_template("public/index.html")
