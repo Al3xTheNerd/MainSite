581ecea92ff40e371e26faffabbc1988da6663c7
Joshua Deason <joshua.deason@zoho.com>
2025-08-29 18:17:41 -0600

    Readd webhook for autodeploy

--- core/routes/public.py
+++ core/routes/public.py
@@ -1,7 +1,8 @@
-from flask import render_template
+from flask import render_template, request
 from core import app
 from core.decorators.auth import admin_required
 from flask_login import login_required
+import git
 
 
 @app.route('/', methods=('GET', 'POST'))
@@ -13,7 +14,12 @@ def mcGradient():
     return render_template("public/mc_gradient.html")
 
 
-@app.route('/test', methods=('GET', 'POST'))
-@admin_required()
-def test(): 
-    return render_template("public/index.html")
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('./MainSite')
+        origin = repo.remotes.origin
+        origin.pull()
+        return '', 200
+    else:
+        return '', 400
