0cd239ee6ab08223fdbb9d63c01f0248c1385979
Joshua Deason <joshua.deason@zoho.com>
2026-08-28 21:59:25 -0600

    rgh

--- core/routes/shopStuff.py
+++ core/routes/shopStuff.py
@@ -517,13 +517,10 @@ def hook():
                     quantity = int(match.group(2))
                     item = match.group(3) + appendStr
                     item = getOrCreateListing(match.group(3) + appendStr, "add", quantity, username=message["username"])
-                    if not match.group(4):
-                        if item.BuyPrice != 0.00:
-                            money = item.BuyPrice * quantity
-                        else:
-                            money = 0.0
+                    if item.BuyPrice != 0.00:
+                        money = item.BuyPrice * quantity
                     else:
-                        money = match.group(4)
+                        money = 0.0
                     db.session.add(ShopLogs(Type = "to", Interactor = name, Quantity = quantity, Item = item.id, TimeStamp = message["time"], ShopOwner=message["username"], Money = money)) # type: ignore
             case "from":
                 pattern = r"(\w+)\s+purchased\s+(\d+)\s+(.+?)\s+from your shop, and you earned\s+\$([\d,]+(?:\.\d+)?)"
